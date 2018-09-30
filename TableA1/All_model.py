eeb620c2f680b513fc8e9984e8f731a65e625b6e
Apple <kmwuhuihang@126.com>
2018-09-30 13:56:07 +0800

    Liner models

--- TableA1/All_model.py
+++ TableA1/All_model.py
@@ -804,188 +804,193 @@ print('GBRT+H Model b OOS:', OOS_score_b * 100)
 save_result(i=13)
 
 
-################################NN1 Model
-
-import tensorflow as tf
-from keras import backend as K
-
-K.clear_session()
-tf.reset_default_graph()
-tf.set_random_seed(1234)
-import gc
-import pandas as pd
-import datetime
-import pickle
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
-from keras import regularizers
-from keras import initializers
-
-from keras.callbacks import ModelCheckpoint
-from keras.models import load_model
-
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
-
-
-def creat_data(df_X, df_Y):
-    '''
-    Data providing function:
-
-    This function is separated from model() so that hyperopt
-    won't reload data for each evaluation run.
-    '''
-    split_num = 200 * 60
-    X_traindata = df_X[:split_num * 2]
-    Y_traindata = df_Y[:split_num * 2]
-    X_vdata = df_X[split_num:split_num * 2]
-    Y_vdata = df_Y[split_num:split_num * 2]
-    X_testdata = df_X[split_num * 2:split_num * 3]
-    Y_testdata = df_Y[split_num * 2:split_num * 3]
-
-    return X_traindata, Y_traindata, X_vdata, Y_vdata, X_testdata, Y_testdata
-
-
-space = {'ll_float': hp.uniform('ll_float', 0.01, 0.2),
-         'lr': hp.loguniform('lr', np.log(0.005), np.log(0.2)),
-         'beta_1_float': hp.uniform('beta_1_float', 0.8, 0.95),
-         'beta_2_float': hp.uniform('beta_2_float', 0.98, 0.9999),
-         'epsilon_float': hp.uniform('epsilon_float', 1e-09, 1e-07),  ##note
-         'batch_size': hp.quniform('batch_size', 5, 500, 1),
-         'epochs': hp.quniform('epochs', 20, 50, 1)
-         }
-
-## set params random search time,when set 50,something will be wrong
-try_num1 = int(50)
-
-
-X_traindata, Y_traindata, X_vdata, Y_vdata, X_testdata, Y_testdata = creat_data(df_X=x_array, df_Y=y1)
+# ################################NN1 Model
+#
+# import tensorflow as tf
+# from keras import backend as K
+#
+# K.clear_session()
+# tf.reset_default_graph()
+# tf.set_random_seed(1234)
+# import gc
+# import pandas as pd
+# import datetime
+# import pickle
+# from keras.models import Sequential
+# from keras.layers.core import Dense, Activation
+# from keras.layers.normalization import BatchNormalization
+# from keras.optimizers import Adam
+# from keras.callbacks import EarlyStopping
+# from keras import regularizers
+# from keras import initializers
+#
+# from keras.callbacks import ModelCheckpoint
+# from keras.models import load_model
+#
+# from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+#
+#
+#
+# def creat_data(df_X, df_Y):
+#     '''
+#     Data providing function:
+#
+#     This function is separated from model() so that hyperopt
+#     won't reload data for each evaluation run.
+#     '''
+#     split_num = 200 * 60
+#     X_traindata = df_X[:split_num * 2]
+#     Y_traindata = df_Y[:split_num * 2]
+#     X_vdata = df_X[split_num:split_num * 2]
+#     Y_vdata = df_Y[split_num:split_num * 2]
+#     X_testdata = df_X[split_num * 2:split_num * 3]
+#     Y_testdata = df_Y[split_num * 2:split_num * 3]
+#
+#     return X_traindata, Y_traindata, X_vdata, Y_vdata, X_testdata, Y_testdata
+#
+#
+# space = {'ll_float': hp.uniform('ll_float', 0.1, 0.2),
+#          'lr': hp.loguniform('lr', np.log(0.008), np.log(0.015)),
+#          'beta_1_float': hp.uniform('beta_1_float', 0.85, 0.95),
+#          'beta_2_float': hp.uniform('beta_2_float', 0.98, 0.9999),
+#          'epsilon_float': hp.uniform('epsilon_float', 1e-09, 1e-07),  ##note
+#          'batch_size': hp.quniform('batch_size', 300, 500, 1),
+#          'epochs': hp.quniform('epochs', 20, 50, 1)
+#          }
+#
+# ## set params random search time,when set 50,something will be wrong
+# try_num1 = int(30)
+#
+#
+# X_traindata, Y_traindata, X_vdata, Y_vdata, X_testdata, Y_testdata = creat_data(df_X=x_array, df_Y=y1)
 
 # define NN1
-def f_NN1(params):
-    split_num = 200 * 60
-    ## define params
-    ll_float = params["ll_float"]  # 0.1
-    learn_rate_float = params["lr"]  # 0.01
-    beta_1_float = params["beta_1_float"]  # 0.9
-    beta_2_float = params["beta_2_float"]  # 0.999
-    epsilon_float = params["epsilon_float"]  # 1e-08
-    batch_size_num = params['batch_size']  #
-    epochs_num = params['epochs']  # 50
-    ## model structure
-    model_NN1 = Sequential()
-    init = initializers.he_normal(seed=100)
-    model_NN1.add(Dense(32, input_dim=len(X_traindata[0]),
-                        activation='relu',
-                        kernel_initializer=init,
-                        kernel_regularizer=regularizers.l1(ll_float)))
-    model_NN1.add(Dense(1))
+# def f_NN1(params):
+#     split_num = 200 * 60
+#     ## define params
+#     ll_float = params["ll_float"]  # 0.1
+#     learn_rate_float = params["lr"]  # 0.01
+#     beta_1_float = params["beta_1_float"]  # 0.9
+#     beta_2_float = params["beta_2_float"]  # 0.999
+#     epsilon_float = params["epsilon_float"]  # 1e-08
+#     batch_size_num = params['batch_size']  #
+#     epochs_num = params['epochs']  # 50
+#     ## model structure
+#     model_NN1 = Sequential()
+#     init = initializers.he_normal(seed=100)
+#     model_NN1.add(Dense(32, input_dim=len(X_traindata[0]),
+#                         activation='relu',
+#                         kernel_initializer=init,
+#                         kernel_regularizer=regularizers.l1(ll_float)))
+#     model_NN1.add(BatchNormalization())
+#     model_NN1.add(Dense(1))
+#
+#     ## comile model
+#     adam = Adam(lr=learn_rate_float, beta_1=beta_1_float, beta_2=beta_2_float, epsilon=epsilon_float)
+#     model_NN1.compile(loss='mse', optimizer=adam)
+#
+#     ## callback fun
+#     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.00001,
+#                                    patience=3, verbose=0, mode='auto')
+#     model_filepath = Path + '\\model\\best_weights.h5'
+#     checkpoint = ModelCheckpoint(filepath=model_filepath, save_weights_only=False,
+#                                  monitor='val_loss', mode='min', save_best_only='True')
+#     callback_lists = [early_stopping, checkpoint]
+#
+#     ## fit model
+#     model_NN1.fit(X_traindata, Y_traindata,
+#                   batch_size=int(batch_size_num),
+#                   epochs=int(epochs_num),
+#                   verbose=0,
+#                   validation_data=(X_vdata, Y_vdata),
+#                   callbacks=callback_lists,
+#                   shuffle=False)
+#
+#     ##get the best model
+#     best_model = load_model(model_filepath)
+#
+#     Y_pre_t = best_model.predict(X_traindata[:split_num], verbose=0)
+#
+#     Y_pre_tlist = []
+#     for x in Y_pre_t[:, 0]:
+#         Y_pre_tlist.append(x)
+#
+#     train_score = r2_score(Y_traindata[:split_num],Y_pre_tlist)
+#
+#     Y_pre_v = best_model.predict(X_vdata, verbose=0)
+#
+#     Y_pre_vlist = []
+#     for x in Y_pre_v[:, 0]:
+#         Y_pre_vlist.append(x)
+#
+#     v_score = r2_score(Y_vdata, Y_pre_vlist)
+#
+#     ## prediction & save
+#     Y_pre = best_model.predict(X_testdata, verbose=0)
+#     Y_pre_list = []
+#     for x in Y_pre[:, 0]:
+#         Y_pre_list.append(x)
+#     test_score = r2_score(Y_testdata, Y_pre_list)
+#
+#     K.clear_session()
+#
+#     return {'loss': -v_score, 'status': STATUS_OK,
+#             'train_score': train_score, 'test_score': test_score}
+#
+#
+# trials = Trials()
+# best = fmin(f_NN1, space, algo=tpe.suggest, max_evals=try_num1, trials=trials)
+#
+#
+# loss_list = trials.losses()
+# min_loss = min(loss_list)
+# for k in range(try_num1):
+#     if min_loss == loss_list[k]:
+#         key = k
+# best_results = trials.results[key]
+#
+# # save model
+#
+#
+# IS_score = best_results['train_score']
+# OOS_score = best_results['test_score']
+#
+# print('NN1 Model a IS:', IS_score * 100)
+# print('NN1 Model a OOS:', OOS_score * 100)
+#
+# K.clear_session()
+# tf.reset_default_graph()
+#
+#
+# X_traindata, Y_traindata, X_vdata, Y_vdata, X_testdata, Y_testdata = creat_data(df_X=x_array, df_Y=y2)
+#
+#
+#
+# trials = Trials()
+# best = fmin(f_NN1, space, algo=tpe.suggest, max_evals=try_num1, trials=trials)
+#
+#
+# loss_list = trials.losses()
+# min_loss = min(loss_list)
+# for k in range(try_num1):
+#     if min_loss == loss_list[k]:
+#         key = k
+# best_results = trials.results[key]
+#
+# # save model
+#
+#
+# IS_score_b = best_results['train_score']
+# OOS_score_b = best_results['test_score']
+#
+# print('NN1 Model b IS:', IS_score_b * 100)
+# print('NN1 Model b OOS:', OOS_score_b * 100)
+#
+# K.clear_session()
+# tf.reset_default_graph()
+# save_result(i=14)
 
-    ## comile model
-    adam = Adam(lr=learn_rate_float, beta_1=beta_1_float, beta_2=beta_2_float, epsilon=epsilon_float)
-    model_NN1.compile(loss='mse', optimizer=adam)
 
-    ## callback fun
-    early_stopping = EarlyStopping(monitor='loss', min_delta=0.00001,
-                                   patience=3, verbose=0, mode='auto')
-    model_filepath = Path + '\\model\\best_weights.h5'
-    checkpoint = ModelCheckpoint(filepath=model_filepath, save_weights_only=False,
-                                 monitor='loss', mode='min', save_best_only='True')
-    callback_lists = [early_stopping, checkpoint]
-
-    ## fit model
-    model_NN1.fit(X_traindata, Y_traindata,
-                  batch_size=int(batch_size_num),
-                  epochs=int(epochs_num),
-                  verbose=0,
-                  validation_data=(X_vdata, Y_vdata),
-                  callbacks=callback_lists,
-                  shuffle=False)
-
-    ##get the best model
-    best_model = load_model(model_filepath)
-
-    Y_pre_t = best_model.predict(X_traindata[:split_num], verbose=0)
-    Y_pre_tlist = []
-    for x in Y_pre_t[:, 0]:
-        Y_pre_tlist.append(x)
-
-    train_score = r2_score(Y_traindata[:split_num],Y_pre_tlist)
-
-
-    Y_pre_v = best_model.predict(X_vdata, verbose=0)
-    Y_pre_vlist = []
-    for x in Y_pre_v[:, 0]:
-        Y_pre_vlist.append(x)
-
-    v_score = r2_score(Y_vdata, Y_pre_vlist)
-
-    ## prediction & save
-    Y_pre = best_model.predict(X_testdata, verbose=0)
-    Y_pre_list = []
-    for x in Y_pre[:, 0]:
-        Y_pre_list.append(x)
-    test_score = r2_score(Y_testdata, Y_pre_list)
-    print('Validate score:', v_score)
-    print('OOS score:',test_score)
-    K.clear_session()
-
-    return {'loss': -v_score, 'status': STATUS_OK,
-            'train_score': train_score, 'test_score': test_score}
-
-
-trials = Trials()
-best = fmin(f_NN1, space, algo=tpe.suggest, max_evals=try_num1, trials=trials)
-
-
-loss_list = trials.losses()
-min_loss = min(loss_list)
-for k in range(try_num1):
-    if min_loss == loss_list[k]:
-        key = k
-best_results = trials.results[key]
-
-# save model
-
-
-IS_score = best_results['train_score']
-OOS_score = best_results['test_score']
-
-print('NN1 Model a IS:', IS_score * 100)
-print('NN1 Model a OOS:', OOS_score * 100)
-
-K.clear_session()
-tf.reset_default_graph()
-
-
-X_traindata, Y_traindata, X_vdata, Y_vdata, X_testdata, Y_testdata = creat_data(df_X=x_array, df_Y=y2)
-
-
-
-trials = Trials()
-best = fmin(f_NN1, space, algo=tpe.suggest, max_evals=try_num1, trials=trials)
-
-
-loss_list = trials.losses()
-min_loss = min(loss_list)
-for k in range(try_num1):
-    if min_loss == loss_list[k]:
-        key = k
-best_results = trials.results[key]
-
-# save model
-
-
-IS_score_b = best_results['train_score']
-OOS_score_b = best_results['test_score']
-
-print('NN1 Model b IS:', IS_score_b * 100)
-print('NN1 Model b OOS:', OOS_score_b * 100)
-
-K.clear_session()
-tf.reset_default_graph()
 
 
 tableA1.to_csv(Path + '//output//tableA1.csv')
